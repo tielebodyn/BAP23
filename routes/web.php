cff4b71916e4427f5709217c8fdba4f0d95963cd
Tiele Bodyn <tiele@esign.eu>
2023-07-30 16:58:59 +0200

    add: Initial navigation routes

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('welcome'); })->name('home');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profiel', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profiel', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profiel', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/kaart', function () { return view('map.index'); })->name('map');
+    Route::get('/aanbiedingen', function () { return view('welcome'); })->name('aanbiedingen');
+    Route::get('/transacties', function () { return view('welcome'); })->name('transacties');
+    Route::get('/groep', function () { return view('welcome'); })->name('groep');
 });
 
 require __DIR__.'/auth.php';
