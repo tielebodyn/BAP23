7e89abc61ee0d50526d8fefad6c93973c39feab4
Tiele Bodyn <tiele@esign.eu>
2023-07-30 16:50:43 +0200

    add: tailwind color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/js/**/*.js',
     ],
 
     theme: {
@@ -15,6 +16,20 @@ export default {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
+        colors: {
+            'primary': '#000000',
+            'primaryB' : '#B6B6B6',
+            'accent': '#7572FF',
+            'danger': '#E72424',
+            'positive': '#2ECC71',
+            'warning': '#F39C12',
+            'black': '#212429',
+            'gray4': '#495057',
+            'gray3': '#7C7D7E',
+            'gray2': '#DDE2E5',
+            'gray1': '#F8F9FA',
+            'white': '#FFFFFF',
+        }
     },
 
     plugins: [forms],
