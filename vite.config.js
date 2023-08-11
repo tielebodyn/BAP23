6c91936ca8403123c13a5f0161019ea01971aed0
Tiele Bodyn <tiele@esign.eu>
2023-08-11 14:50:28 +0200

    Add: js files for build testing

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                // load every js file individually
+                'resources/js/pages/edit-profile.js',
+                'resources/js/components/sidebar.js',
             ],
             refresh: true,
         }),
