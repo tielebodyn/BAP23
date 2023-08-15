e160d3f40440e9f16a0cfc6ba36d43beb2a2266d
Tiele Bodyn <tiele@esign.eu>
2023-08-15 13:10:26 +0200

    Add: accept,decline,edit,update methods

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateGroupRequest;
+use App\Http\Requests\Group\UpdateGroupRequest;
 use App\Models\Group;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -30,7 +31,7 @@ class GroupController extends Controller
         $group = Group::create($groupData);
         // make user admin
         $group->users()->attach($request->user()->id, ['role' => 'admin']);
-
+        $group->users()->updateExistingPivot($request->user()->id, ['status' => 'accepted']);
         if ($request->file('logo')) {
             $image = $request->file('logo');
             $imageName = 'groupImage' . '.' . $image->extension();
@@ -42,6 +43,49 @@ class GroupController extends Controller
         }
         // redirect to group page
         Session::put('currentGroup', $group->id);
+        return redirect()->route('group.dashboard', $group);
+    }
+    // accept
+    public function accept(Group $group, Request $request)
+    {
+        // update group user status to accepted
+        $group->users()->updateExistingPivot($request->user()->id, ['status' => 'accepted']);
+        // back to same page with group
+        return redirect()->back();
+    }
+    // decline
+    public function decline(Group $group, Request $request)
+    {
+        // update group user status to declined
+        $group->users()->updateExistingPivot($request->user()->id, ['status' => 'declined']);
+        // redirect to home page
+        return redirect()->back();
+    }
+
+    // edit
+    public function edit(Group $group)
+    {
+        return view('group.admin.edit', compact('group'));
+    }
+    // update
+    public function update(UpdateGroupRequest $request, Group $group)
+    {
+        // validate
+
+        $groupData = $request->validated();
+        // update group
+        $group->update($groupData);
+        // update logo
+        if ($request->file('logo')) {
+            $image = $request->file('logo');
+            $imageName = 'groupImage' . '.' . $image->extension();
+            $relativePath = 'storage/images/groups/' . $group->id;
+            $absolutePath = public_path('storage/images/groups/' . $group->id);
+            $image->move($absolutePath, $imageName);
+            $groupData['logo'] = $relativePath . '/' . $imageName;
+            $group->update($groupData);
+        }
+        // redirect to group page
         return redirect()->route('group.dashboard', $group);
     }
 }
